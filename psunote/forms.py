9bab734afc00b90a98730207c9eb1eddf8ebed47
ธรากร ชลายนคุปต์ <meowkhangban@gmail.com>
2023-10-09 10:53:10 +0700

    Create TagFrom from model tags

--- psunote/forms.py
+++ psunote/forms.py
@@ -38,6 +38,13 @@ BaseNoteForm = model_form(
     models.Note, base_class=FlaskForm, exclude=["created_date", "updated_date"]
 )
 
+BaseTagForm = model_form(
+    models.Tag, base_class=FlaskForm, exclude=["created_date", "updated_date"]
+)
+
 
 class NoteForm(BaseNoteForm):
+    tags = TagListField("Tag")
+
+class TagForm(BaseTagForm):
     tags = TagListField("Tag")
